031b8043ad1b4755a3a9f2c2dd76bf0723c3dc55
metzmom <84488543+metzmom@users.noreply.github.com>
2022-01-03 16:51:43 -0600

    Add SpringBoot application.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Dserver.port=$PORT $JAVA_OPTS -jar build/libs/MicrogreenMargie-1.0-SNAPSHOT.jar
---
+++ src/main/java/org/example/controllers/HelloController.java
@@ -0,0 +1,12 @@
+package org.example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/")
+    public String sayHello(){
+        return "index";
+    }
+}
